6a0c4583d482fbaaa82980bacfd73f3d8173ca6e
Flavio Mendes <fhenrique609@gmail.com>
2021-10-11 16:48:03 -0300

    Estilizando o SafeEnvironmentContainer

---
+++ src/ui/components/feedback/SafeEnvironment/SafeEnvironment.style.tsx
@@ -0,0 +1,7 @@
+import { experimentalStyled as styled } from '@mui/material'
+
+export const SafeEnvironmentContainer = styled('div')`
+    text-align: right;
+    padding: 16px 0px;
+    font-size: 12px;
+`
