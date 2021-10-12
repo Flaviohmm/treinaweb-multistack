c20a3bd5360ee4f58bf11d07d7cc1049d4e5695f
Flavio Mendes <fhenrique609@gmail.com>
2021-10-12 10:21:01 -0300

    TextField, TextFieldMask in FormElementsContainer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,39 @@
 import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment'
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle'
 import UserInformation from 'ui/components/data-display/UserInformation/UserInformation'
+import TextFieldMask from 'ui/components/inputs/TextFieldMask/TextFieldMask'
+import { Button, Typography } from '@mui/material'
+import { FormElementsContainer } from '@styles/pages/index.style'
 
 export default function Home() {
   return (
     <div>
       <SafeEnvironment />
+
       <PageTitle 
         title={'Conheça os profissionais'}
         subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
       />
+
+      <FormElementsContainer>
+        <TextFieldMask
+          mask={"99.999-999"}
+          label={"Digite seu CEP"}
+          fullWidth
+          variant={"outlined"}
+        />
+
+        <Typography color={"error"}>CEP Inválido</Typography>
+
+        <Button
+          variant={"contained"}
+          color={"secondary"}
+          sx={{ width: "220px"}}
+        >
+          Buscar
+        </Button>
+      </FormElementsContainer>
+
       <UserInformation
         name={"Flavio Mendes"}
         picture={"https://github.com/Flaviohmm.png"}
